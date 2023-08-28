098ddd199265085c2516eb88aacf2067ff263a65
hexpaydaydev <136479589+hexpaydaydev@users.noreply.github.com>
2023-08-28 16:16:25 -0500

    Upps baseline wallets

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -58,7 +58,7 @@ const defaultHardhatNetwork = {
   ...defaultNetwork,
   accounts: {
     accountsBalance: ethers.utils.parseEther((1_000_000_000).toString()).toString(),
-    count: 3,
+    count: 5,
     mnemonic: conf.args.mnemonic,
   } as HardhatNetworkAccountsConfig,
 }
