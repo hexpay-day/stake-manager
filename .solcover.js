7e399a0749df8b690d9baa08a697bd91a57a78c2
hexpaydaydev <136479589+hexpaydaydev@users.noreply.github.com>
2023-08-28 22:09:12 -0500

    Prep for coverage

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,25 @@
 module.exports = {
   configureYulOptimizer: true,
+  solcOptimizerDetails: {
+    peephole: false,
+    inliner: false,
+    jumpdestRemover: false,
+    orderLiterals: true,  // <-- TRUE! Stack too deep when false
+    deduplicate: false,
+    cse: false,
+    constantOptimizer: false,
+    yul: false
+  },
+  measureStatementCoverage: false,
   skipFiles: [
     'reference',
     'interfaces',
-  ]
+  ],
+  providerOptions: {
+    default_balance_ether: '1000000000000000000000000000',
+  },
+  mocha: {
+    fgrep: '[skip-on-coverage]',
+    invert: true,
+  },
 };
