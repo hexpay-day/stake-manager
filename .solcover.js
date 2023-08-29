131ace2356aff7a1795c8a228b80be35e4bc0d07
hexpaydaydev <136479589+hexpaydaydev@users.noreply.github.com>
2023-08-28 22:25:25 -0500

    Remove reference to pnpm

--- .solcover.js
+++ .solcover.js
@@ -1,25 +1,25 @@
 module.exports = {
   configureYulOptimizer: true,
-  solcOptimizerDetails: {
-    peephole: false,
-    inliner: false,
-    jumpdestRemover: false,
-    orderLiterals: true,  // <-- TRUE! Stack too deep when false
-    deduplicate: false,
-    cse: false,
-    constantOptimizer: false,
-    yul: false
-  },
-  measureStatementCoverage: false,
   skipFiles: [
     'reference',
     'interfaces',
   ],
-  providerOptions: {
-    default_balance_ether: '1000000000000000000000000000',
-  },
-  mocha: {
-    fgrep: '[skip-on-coverage]',
-    invert: true,
-  },
+  // solcOptimizerDetails: {
+  //   peephole: false,
+  //   inliner: false,
+  //   jumpdestRemover: false,
+  //   orderLiterals: true,  // <-- TRUE! Stack too deep when false
+  //   deduplicate: false,
+  //   cse: false,
+  //   constantOptimizer: false,
+  //   yul: false
+  // },
+  // measureStatementCoverage: false,
+  // providerOptions: {
+  //   default_balance_ether: '1000000000000000000000000000',
+  // },
+  // mocha: {
+  //   fgrep: '[skip-on-coverage]',
+  //   invert: true,
+  // },
 };
