9eebd7ed89d0d0a1111b053a8eb29bc5034bbe6e
hexpaydaydev <136479589+hexpaydaydev@users.noreply.github.com>
2023-10-02 09:17:19 -0500

    Adds skip for integration test

--- test/2023-10-02.test.ts
+++ test/2023-10-02.test.ts
@@ -9,10 +9,14 @@ import { parseLogs } from '../src/utils'
 import { StakeEndEvent } from '../artifacts/types/contracts/interfaces/IHEX'
 
 describe("2023-10-02 utc", function () {
-  it('can end base and hsi', async () => {
+  it('can end base and hsi', async function () {
     const depositDate = new Date('2023-10-01T00:00:00Z')
     const endDate = new Date('2023-10-02T00:00:00Z')
     const execAddress = '0xCF1500783F82867C8600bfa3023b9828F4a5a870'
+
+    if (+depositDate < _.now()) {
+      this.skip()
+    }
 
     const x = await loadFixture(utils.deployFixture)
     const hour = 60*60
@@ -20,27 +24,27 @@ describe("2023-10-02 utc", function () {
 
     const endTime = Math.floor(+endDate / 1_000)
     const deltaDays = Math.floor(((endTime - depositTime) * 1_000) / utils.DAY) - 1
-    const hsiAddresses = [
-      '0xd9ebc58E0fFf9741c7749c215bb36616928e7c04',
-      '0xca443C68702f789550558892c568B57ACED9eaC8',
-      '0xc531C6aa0CBead6376c369084EED92173FdEb838',
-      '0x3dd5410e739e26a46C477311a69D24544761D91b',
-      '0xC3C2a22036C06e94B1fD08F0eB413F2F9cB681c8',
-      '0x245b3eD19870Def1f1E68880130a0d2b352E5D27',
+    const tokenIds = [
+      14095,
+      14096,
+      14097,
+      14098,
+      14099,
+      14100,
     ]
-    // const hsis = _.compact(await Promise.all(tokenIds.map(async (tokenId) => {
-    //   const hsiAddress = await x.hsim.hsiToken(tokenId)
-    //   if (hsiAddress === hre.ethers.constants.AddressZero) {
-    //     // skip case where token is not a contract
-    //     return
-    //   }
-    //   const stake = await x.hex.stakeLists(hsiAddress, 0)
-    //   return {
-    //     tokenId,
-    //     hsiAddress,
-    //     stakeId: stake.stakeId,
-    //   }
-    // })))
+    const hsis = _.compact(await Promise.all(tokenIds.map(async (tokenId) => {
+      const hsiAddress = await x.hsim.hsiToken(tokenId)
+      if (hsiAddress === hre.ethers.constants.AddressZero) {
+        // skip case where token is not a contract
+        return
+      }
+      const stake = await x.hex.stakeLists(hsiAddress, 0)
+      return {
+        tokenId,
+        hsiAddress,
+        stakeId: stake.stakeId,
+      }
+    })))
     await hre.vizor.impersonate(execAddress, async (swa) => {
       const execStakeManager = x.existingStakeManager.connect(swa)
       if (_.now() < depositTime) {
@@ -63,7 +67,7 @@ describe("2023-10-02 utc", function () {
         return execStakeManager.multicall(
           [
             execStakeManager.interface.encodeFunctionData('stakeEndAs', [swa.address, x.base, stake.stakeId]),
-            execStakeManager.interface.encodeFunctionData('hsiStakeEndMany', [hsiAddresses]),
+            execStakeManager.interface.encodeFunctionData('hsiStakeEndMany', [_.map(hsis, 'hsiAddress')]),
           ],
           false,
         )
@@ -82,16 +86,16 @@ describe("2023-10-02 utc", function () {
       await time.setNextBlockTimestamp(endTime)
       lastBlockTime = await getLastBlockTime()
       console.log('ending', lastBlockTime, new Date(endTime * 1_000), await x.existingStakeManager.checkEndable(x.base))
-      const stakeListResults = await x.multicall.callStatic.aggregate3(hsiAddresses.map((hsi) => ({
-        value: 0,
-        allowFailure: false,
-        target: x.hex.address,
-        callData: x.hex.interface.encodeFunctionData('stakeLists', [hsi, 0]),
-      })))
-      const stakeIds = stakeListResults.map((result) => (
-        (x.hex.interface.decodeFunctionResult('stakeLists', result.returnData)[0] as IUnderlyingStakeable.StakeStoreStructOutput).stakeId
-      ))
-      const hsiToStakeLists = new Map<string, number>(_.zip(hsiAddresses, stakeIds) as [string, number][])
+      // const stakeListResults = await x.multicall.callStatic.aggregate3(hsiAddresses.map((hsi) => ({
+      //   value: 0,
+      //   allowFailure: false,
+      //   target: x.hex.address,
+      //   callData: x.hex.interface.encodeFunctionData('stakeLists', [hsi, 0]),
+      // })))
+      // const stakeIds = stakeListResults.map((result) => (
+      //   (x.hex.interface.decodeFunctionResult('stakeLists', result.returnData)[0] as IUnderlyingStakeable.StakeStoreStructOutput).stakeId
+      // ))
+      // const hsiToStakeLists = new Map<string, number>(_.zip(hsiAddresses, stakeIds) as [string, number][])
       const doingCalls = doCalls()
 
       await expect(doingCalls)
@@ -100,23 +104,10 @@ describe("2023-10-02 utc", function () {
         // .printGasUsage()
       lastBlockTime = await getLastBlockTime()
       console.log('ending', lastBlockTime, new Date(endTime * 1_000), await x.existingStakeManager.checkEndable(x.base))
-      // console.log(hsiAddresses, hsiToStakeLists)
-      // const tx = await doingCalls
-      // const receipt = await tx.wait()
-      // const logs = parseLogs([x.hex.interface], receipt.logs)
-      // console.log(logs.valid.filter((log) => {
-      //   const stakeEnd: StakeEndEvent = log as any
-      //   if (log.name != 'StakeEnd') return
-      //   const penalty = BigInt.asUintN(72, stakeEnd.args.data1.toBigInt())
-      //   console.log(stakeEnd.args.stakeId, penalty)
-      // }))
-      for (let hsi of hsiAddresses) {
-        // console.log(hsi)
+      for (let hsi of hsis) {
         await expect(doingCalls)
           .to.emit(x.hex, 'StakeEnd')
-          .withArgs(anyUint, utils.anyUintNoPenalty, x.base, stake.stakeId)
-          .to.emit(x.hex, 'StakeEnd')
-          .withArgs(anyUint, utils.anyUintNoPenalty, hsi, hsiToStakeLists.get(hsi))
+          .withArgs(anyUint, utils.anyUintNoPenalty, hsi.hsiAddress, hsi.stakeId)
       }
       const IPoolContract = await hre.ethers.getContractAt('IPoolContract', x.base, swa)
       await expect(IPoolContract.getEndStaker())
