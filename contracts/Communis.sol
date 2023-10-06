53994ad5744d583423927bace593e527256b3fd4
hexpaydaydev <136479589+hexpaydaydev@users.noreply.github.com>
2023-10-06 09:40:07 -0500

    Adds layers to communis methods

--- contracts/Communis.sol
+++ contracts/Communis.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.18;
 
-abstract contract Communis {
+import { ERC20 } from "solmate/src/tokens/ERC20.sol";
+
+abstract contract Communis is ERC20 {
   struct Stake {
     uint256 stakeID;
     uint256 stakedHearts;
@@ -30,5 +32,13 @@ abstract contract Communis {
 
   function withdrawStakedCodeak(uint256 withdrawAmount) external virtual;
   function getPayout(Stake memory s) external virtual pure returns (PayoutResponse memory pr);
-  function getStartBonusPayout(uint256 stakedDays, uint256 lockedDay, uint256 maxPayout, uint256 stakesOriginalShareRate, uint256 currentDay, uint256 globalShareRate, bool applyRestakeBonus) external virtual pure returns (uint256 payout); 
+  function getStartBonusPayout(
+    uint256 stakedDays,
+    uint256 lockedDay,
+    uint256 maxPayout,
+    uint256 stakesOriginalShareRate,
+    uint256 currentDay,
+    uint256 globalShareRate,
+    bool applyRestakeBonus
+  ) external virtual pure returns (uint256 payout);
 }
--- test/Communis/SingletonCommunis.test.ts
+++ test/Communis/SingletonCommunis.test.ts
@@ -1,57 +1,53 @@
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers"
 import { expect } from "chai"
+import * as hre from 'hardhat'
 import * as utils from '../utils'
 import _ from 'lodash'
+import { fromStruct } from "../../src/utils"
 
 describe('SingletonCommunis.sol', () => {
   describe('withdrawAmountByStakeId', () => {
     it('Communis withdraw', async () => {
       const x = await loadFixture(utils.deployFixture)
-      const [signer1] = x.signers
-
-      await x.stakeManager.connect(signer1).stakeStart(x.stakedAmount, 180)
+      await x.stakeManager.stakeStart(x.stakedAmount, 180)
       await utils.moveForwardDays(1n, x)
 
-      const addressStakeCount = await x.stakeManager.stakeCount(await x.stakeManager.getAddress());
+      const addressStakeCount = await x.stakeManager.stakeCount(x.stakeManager.getAddress())
 
-      const stake = await x.hex.stakeLists(await x.stakeManager.getAddress(), addressStakeCount - 1n)
+      const stake = await x.hex.stakeLists(x.stakeManager.getAddress(), addressStakeCount - 1n)
 
-      const [stakeId, stakedHearts, stakeShares, lockedDay, stakedDays, unlockedDay, isAutoStake] = stake;
+      const stk = fromStruct(stake)
 
-      const stakeStoreObject = {
-        stakeId: Number(stakeId),
-        stakedHearts: Number(stakedHearts),
-        stakeShares: Number(stakeShares),
-        lockedDay: Number(lockedDay),
-        stakedDays: Number(stakedDays),
-        unlockedDay: Number(unlockedDay),
-        isAutoStake
-      };
+      let payoutResponse = await x.communis.getPayout({
+        ...stk,
+        stakeID: stk.stakeId,
+      })
+      let globalInfo = await x.hex.globalInfo()
+      let startBonusPayout = await x.communis.getStartBonusPayout(
+        stk.stakedDays,
+        stk.lockedDay,
+        payoutResponse.maxPayout,
+        payoutResponse.stakesOriginalShareRate,
+        await x.hex.currentDay(),
+        globalInfo[2],
+        false,
+      )
 
-      const stakeObject = {
-        stakeID: Number(stakeId),
-        stakedHearts: Number(stakedHearts),
-        stakeShares: Number(stakeShares),
-        lockedDay: Number(lockedDay),
-        stakedDays: Number(stakedDays),
-        unlockedDay: Number(unlockedDay)
-      };
+      await x.stakeManager.mintCommunis(
+        0n, stk.stakeId,
+        hre.ethers.ZeroAddress,
+        startBonusPayout,
+      )
 
-      let payoutResponse = await x.communis.getPayout(stakeObject);
-      let globalInfo = await x.hex.globalInfo();
-      let startBonusPayout = await x.communis.getStartBonusPayout(stakedDays, lockedDay, payoutResponse.maxPayout, payoutResponse.stakesOriginalShareRate, await x.hex.currentDay(), globalInfo[2], false);
-  
-      await x.stakeManager.mintCommunis(0n, stakeId, "0x0000000000000000000000000000000000000000", startBonusPayout);
+      await expect(x.communis.stakeIdStartBonusPayout(stk.stakeId))
+        .eventually.to.equal(startBonusPayout)
+      await expect(x.stakeManager.stakeIdStakedAmount(stk.stakeId))
+        .eventually.to.equal(startBonusPayout)
 
-      await expect(x.communis.stakeIdStartBonusPayout(stakeId))
-          .eventually.to.equal(startBonusPayout)
-      await expect(x.stakeManager.stakeIdStakedAmount(stakeId))
-          .eventually.to.equal(startBonusPayout);
+      await x.stakeManager.withdrawAmountByStakeId(startBonusPayout, stk.stakeId)
 
-      await x.stakeManager.withdrawAmountByStakeId(startBonusPayout, stakeStoreObject);
-
-      await expect(x.stakeManager.stakeIdStakedAmount(stakeId))
-          .eventually.to.equal(0);
+      await expect(x.stakeManager.stakeIdStakedAmount(stk.stakeId))
+        .eventually.to.equal(0)
     })
   })
 })
