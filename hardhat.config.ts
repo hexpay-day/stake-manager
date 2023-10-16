d05aa9bdbf0f01e03e09bb554e6dd134a8ef99db
hexpaydaydev <136479589+hexpaydaydev@users.noreply.github.com>
2023-10-16 12:03:08 -0500

    Increases optimizer runs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -206,7 +206,7 @@ const networks: Record<string, NetworkUserConfig> = {
 const settings: SolcUserConfig["settings"] = {
   optimizer: {
     enabled: true,
-    runs: 200,
+    runs: 2000,
     // details: {
     //   yul: true,
     //   yulDetails: {
